68a6d3be8002e2477a8e1a543d1ee2905ee42177
PokyYoky <e-text@online.de>
2022-07-19 11:20:12 +0200

    icon links updated

--- src/app/projects.ts
+++ src/app/projects.ts
@@ -12,7 +12,7 @@ export const projects = [
         title: "El Pollo Loco",
         url: "https://andreas-komor.developerakademie.net/Projekte/ElPolloLoco/index.html",
         description: "A crazy jump & run game in the desert of Mexico...",
-        image: "../assets/img/elpolloloco_640.jpg"
+        image: "./assets/img/elpolloloco_640.jpg"
     },
     {
         category: "",
@@ -26,6 +26,6 @@ export const projects = [
         title: "Join",
         url: "",
         description: "A kanban board to support SCRUM",
-        image: "../assets/img/join_640.jpg"
+        image: "./assets/img/join_640.jpg"
     }
 ];
