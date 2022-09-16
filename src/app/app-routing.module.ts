864fb8d5831f68850e2f5e4d63ad05c9c29a7f46
PokyYoky <e-text@online.de>
2022-09-16 10:41:19 +0200

    imprint component as link embedded

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ImprintComponent } from './imprint/imprint.component';
-import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 const routes: Routes = [
-  {path: '', component: AppComponent},
+  {path: '', component: MainPageComponent},
   {path: 'imprint', component: ImprintComponent}
 ];
 
